bebe1fc3c3c8079cfd57ccf0c1b9ee02bdf00dd4
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-04-04 15:02:14 +0900

    시작화면->로그인 화면 intent, 로그인 화면 구현 예정

--- app/src/main/java/com/ihh/capstone/OTP/StartActivity.java
+++ app/src/main/java/com/ihh/capstone/OTP/StartActivity.java
@@ -6,32 +6,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.ihh.capstone.R;
-import com.ihh.capstone.databinding.ActivityStartBinding;
-
 public class StartActivity extends AppCompatActivity {
-    public ActivityStartBinding binding;
     Context context;
-
+    Button btn_goLoginPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        binding = ActivityStartBinding.inflate(getLayoutInflater());
+        btn_goLoginPage = findViewById(R.id.btn_goLoginPage);
 
         goLoginPage();
+
     }
-
-
     private void goLoginPage() {
-        binding.btnGoLoginPage.setOnClickListener(new View.OnClickListener() {
+        btn_goLoginPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, LoginActivity.class);
                 startActivity(intent);
-                Toast.makeText(context, "로그인 화면으로 이동합니다.", Toast.LENGTH_LONG).show();
             }
         }
         );
