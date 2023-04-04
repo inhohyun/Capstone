1c1bc11e3a5d9d1f2f840279881ee357572ea322
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-04-04 14:26:32 +0900

    시작화면 수정(이미지 -> 텍스트)

--- app/src/main/java/com/ihh/capstone/OTP/StartActivity.java
+++ app/src/main/java/com/ihh/capstone/OTP/StartActivity.java
@@ -2,13 +2,18 @@ package com.ihh.capstone.OTP;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.ihh.capstone.R;
 import com.ihh.capstone.databinding.ActivityStartBinding;
 
 public class StartActivity extends AppCompatActivity {
     public ActivityStartBinding binding;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,16 @@ public class StartActivity extends AppCompatActivity {
         goLoginPage();
     }
 
-    private void goLoginPage() {
 
+    private void goLoginPage() {
+        binding.btnGoLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(context, "로그인 화면으로 이동합니다.", Toast.LENGTH_LONG).show();
+            }
+        }
+        );
     }
 }
