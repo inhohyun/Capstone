a70ee090ccc7191dff80dc4d9b826302f50482c3
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-04-11 21:11:53 +0900

    opt 로그인 서비스 ui 구성

---
+++ app/src/main/java/com/ihh/capstone/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.ihh.capstone.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ihh.capstone.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
