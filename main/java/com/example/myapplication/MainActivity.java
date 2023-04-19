80ddb8eb43a45191e9551ae38edbc34c52bbaf0a
hansol72 <117181640+hansol72@users.noreply.github.com>
2023-04-20 00:40:50 +0900

    갤러리에서 사진 불러오기
    사진 불러와서 비트맵형식으로 보여줌

--- main/java/com/example/myapplication/MainActivity.java
+++ main/java/com/example/myapplication/MainActivity.java
@@ -4,64 +4,62 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_picture;
-    private Bitmap imageBitmap;
-    private ImageView imageView;
-    private static final int REQUEST_IMAGE_CODE = 101;
-    private final ActivityResultLauncher<Intent> imageCaptureLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == RESULT_OK) {
-                    Intent data = result.getData();
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    imageView.setImageBitmap(imageBitmap);
-                }
-            }
-    );
+    Uri uri;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageview);
+        Button selectImageBtn = findViewById(R.id.selectImageBtn);
+        imageView = findViewById(R.id.imageView);
 
-        btn_picture = findViewById(R.id.btn_picture);
-        btn_picture.setOnClickListener(new View.OnClickListener() {
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                takepicture();
-
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*"); //이미지 유형 지정
+                startActivityResult.launch(intent);
             }
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (imageBitmap != null) {
-            imageBitmap.recycle();
-            imageBitmap = null;
-        }
-    }
-    public void takepicture() {
-        Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//카메라 앱 호출
-        if (imageIntent.resolveActivity(getPackageManager()) != null) {
-            imageCaptureLauncher.launch(imageIntent);
-        }
-    }
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult( //선택된 이미지 처리
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if( result.getResultCode() == RESULT_OK && result.getData() != null){
+
+                        uri = result.getData().getData();
+
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                            imageView.setImageBitmap(bitmap);
+
+                        }catch (FileNotFoundException e){
+                            e.printStackTrace();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
 }
