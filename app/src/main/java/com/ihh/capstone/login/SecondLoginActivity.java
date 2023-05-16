19e7a13331ceac6f5013d7913c487b55d6c6cfdc
인호현 <96523102+inhohyun@users.noreply.github.com>
2023-05-17 06:23:28 +0900

    2차 로그인 인증 구현중

--- app/src/main/java/com/ihh/capstone/login/SecondLoginActivity.java
+++ app/src/main/java/com/ihh/capstone/login/SecondLoginActivity.java
@@ -3,14 +3,30 @@ package com.ihh.capstone.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ihh.capstone.R;
 
 public class SecondLoginActivity extends AppCompatActivity {
+    EditText phoneNum;
+    Button btnSendSMS;
+
+    EditText inputCheckNum;
+    Button btnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_login);
+        init();
+    }
+
+    private void init(){
+        phoneNum = findViewById(R.id.phoneNumberArea);
+        btnSendSMS = findViewById(R.id.btn_sendSMS);
+
+        inputCheckNum = findViewById(R.id.checkNumberArea);
+        btnCheck = findViewById(R.id.secondLoginBtn);
     }
 }
